0e4f3fedb7820d2e7fc6ab1e298db2a16bb1baaa
Dmitry Marov <d.marov94@gmail.com>
2023-01-06 23:11:35 +0300

    100 doors wip

---
+++ README.txt
@@ -0,0 +1,1 @@
+# Collection of examples for Behavior Driven Development unit testing approach
--- src/cyber-dojo/100doors/100doors.spec.ts
+++ src/cyber-dojo/100doors/100doors.spec.ts
@@ -3,7 +3,7 @@ import { DoorState } from './door-state';
 import { ProblemParams } from './problem-params';
 
 describe('100doors', () => {
-  it('should return correct answer', () => {
+  it('should return correct answer for 1 door and 1 iteration', () => {
     const params: ProblemParams = {
       numberOfDoors: 1,
       numberOfIterations: 1,
