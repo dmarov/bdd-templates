4fa116158187d1718dd26075fc713b2dcd8925c8
Dmitry Marov <d.marov94@gmail.com>
2023-01-08 19:59:04 +0300

    cycle problem wip

--- src/cycle-problem/index.ts
+++ src/cycle-problem/index.ts
@@ -1,12 +1,26 @@
-import { CycleProblem } from './cycle-problem';
+// import { CycleProblem } from './cycle-problem';
 
-const fn = `
-if (x % 2 === 1) {
-  return (3) * x + (1)
-}
+// const fn = `
+// if (x % 2 === 1) {
+//   return (3) * x + (1)
+// }
 
-return (1 / 2) * x`;
+// return (1 / 2) * x`;
 
-const result = CycleProblem.resolve(fn, 100);
+// const result = CycleProblem.resolve(fn, 100);
 
-console.log(result);
+// console.log(result);
+//
+
+
+const fn = (x: number) => {
+  switch (x % 2) {
+    case 1:
+      return (3 / 1) * x + (1 / 1);
+    default:
+      return (1/ 2) * x + (0 / 1);
+  }
+};
+
+console.log(fn(1));
+console.log(fn(2));
