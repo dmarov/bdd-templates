767df80ed28d142bc7877b7a1a96f2cf53c2d152
Dmitry Marov <d.marov94@gmail.com>
2023-01-08 20:02:59 +0300

    cycle problem wip

--- src/cycle-problem/index.ts
+++ src/cycle-problem/index.ts
@@ -12,13 +12,12 @@
 // console.log(result);
 //
 
-
 const fn = (x: number) => {
   switch (x % 2) {
     case 1:
       return (3 / 1) * x + (1 / 1);
     default:
-      return (1/ 2) * x + (0 / 1);
+      return (1 / 2) * x + (0 / 1);
   }
 };
 
