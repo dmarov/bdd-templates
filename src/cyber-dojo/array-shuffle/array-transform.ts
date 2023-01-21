ee02e51df70a8f8fbc7b0376309265ab146c87bd
Dmitry Marov <d.marov94@gmail.com>
2023-01-21 22:37:01 +0300

    array shuffle wip

--- src/cyber-dojo/array-shuffle/array-transform.ts
+++ src/cyber-dojo/array-shuffle/array-transform.ts
@@ -1,8 +1,5 @@
 export class ArrayTransform {
   static transform<T>(array: T[], transformation: number[]): T[] {
-    const result: T[] = Array.from({ length: array.length })
-      .map((v, i) => array[transformation[i]]);
-
-    return result;
+    return [...array].map((v, i) => array[transformation[i]]);
   }
 }
