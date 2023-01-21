ec1cb4a96e5773f39c9a323dbce1b923d7da5528
Dmitry Marov <d.marov94@gmail.com>
2023-01-21 22:28:11 +0300

    array shuffle wip

--- src/cyber-dojo/array-shuffle/array-transform.spec.ts
+++ src/cyber-dojo/array-shuffle/array-transform.spec.ts
@@ -3,7 +3,7 @@ import { ArrayTransform } from './array-transform';
 describe('array transform', () => {
   it('should work correctly for array of one element and mirror transformatoin', () => {
     const input = [1];
-    const transformation = [1];
+    const transformation = [0];
 
     const result = ArrayTransform.transform(input, transformation);
 
@@ -13,11 +13,21 @@ describe('array transform', () => {
 
   it('should work correctly for array of two elements and mirror transformatoin', () => {
     const input = [1, 2];
-    const transformation = [1, 2];
+    const transformation = [0, 1];
 
     const result = ArrayTransform.transform(input, transformation);
 
     const expected = [1, 2];
+    expect(result).toEqual(expected);
+  });
+
+  it('should work correctly for array of two elements and reverse transformatoin', () => {
+    const input = [1, 2];
+    const transformation = [1, 0];
+
+    const result = ArrayTransform.transform(input, transformation);
+
+    const expected = [2, 1];
     expect(result).toEqual(expected);
   });
 });
