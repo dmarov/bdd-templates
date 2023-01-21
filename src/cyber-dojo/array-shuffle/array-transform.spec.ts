48383c672537d0f732f42320aaa017663e2ae806
Dmitry Marov <d.marov94@gmail.com>
2023-01-21 22:27:24 +0300

    array shuffle wip

--- src/cyber-dojo/array-shuffle/array-transform.spec.ts
+++ src/cyber-dojo/array-shuffle/array-transform.spec.ts
@@ -1,13 +1,23 @@
 import { ArrayTransform } from './array-transform';
 
 describe('array transform', () => {
-  it('should work correctly for array of one element', () => {
+  it('should work correctly for array of one element and mirror transformatoin', () => {
     const input = [1];
     const transformation = [1];
 
     const result = ArrayTransform.transform(input, transformation);
 
     const expected = [1];
+    expect(result).toEqual(expected);
+  });
+
+  it('should work correctly for array of two elements and mirror transformatoin', () => {
+    const input = [1, 2];
+    const transformation = [1, 2];
+
+    const result = ArrayTransform.transform(input, transformation);
+
+    const expected = [1, 2];
     expect(result).toEqual(expected);
   });
 });
