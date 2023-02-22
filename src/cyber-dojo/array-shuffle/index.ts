99fad20d54382837f67874658bc22fa8cc9f661f
Dmitry Marov <d.marov94@gmail.com>
2023-02-22 22:29:40 +0300

    array shuffle done

--- src/cyber-dojo/array-shuffle/index.ts
+++ src/cyber-dojo/array-shuffle/index.ts
@@ -1,8 +1,17 @@
 import { ArrayShuffle } from './array-shuffle/array-shuffle';
 import { MathGenerator } from './normalized-generators/math';
 
-const array = Array.from({ length: 100 }).map((v, i) => i);
-const instance = new ArrayShuffle(() => new MathGenerator());
-const result = instance.shuffle(array);
+const steps = 1000;
+const totals = Array.from({ length: 100 }).map(() => 0);
 
-console.log(result);
+for (let i = 0; i < steps; i += 1) {
+  const array = Array.from({ length: 100 }).map((v, j) => j);
+  const instance = new ArrayShuffle(() => new MathGenerator());
+  const result = instance.shuffle(array);
+
+  for (let k = 0; k < 100; k += 1) {
+    totals[k] += result[k];
+  }
+}
+
+console.log(totals);
