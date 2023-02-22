fcf7aabfae6be19b436148d3c16cdca77f356deb
Dmitry Marov <d.marov94@gmail.com>
2023-02-22 22:16:18 +0300

    array shuffle done

--- src/cyber-dojo/array-shuffle/array-transform-generator/array-transform-generator.ts
+++ src/cyber-dojo/array-shuffle/array-transform-generator/array-transform-generator.ts
@@ -11,14 +11,12 @@ export class ArrayTransformGenerator {
     const arr = Array.from({ length }).map((v, i) => i);
 
     for (let i = 0; i < arr.length; i += 1) {
-      for (let j = i + 1; j < arr.length; j += 1) {
-        const rnd = rangeGen.generate(0, 2);
+      for (let j = i; j < arr.length; j += 1) {
+        const k = rangeGen.generate(j, arr.length);
 
-        if (rnd === 1) {
-          const buf = arr[i];
-          arr[i] = arr[j];
-          arr[j] = buf;
-        }
+        const buf = arr[k];
+        arr[k] = arr[j];
+        arr[j] = buf;
       }
     }
 
--- src/cyber-dojo/array-shuffle/index.ts
+++ src/cyber-dojo/array-shuffle/index.ts
@@ -1,9 +1,8 @@
 import { ArrayShuffle } from './array-shuffle/array-shuffle';
 import { MathGenerator } from './normalized-generators/math';
 
-const array = [1, 2, 3, 4, 5];
+const array = Array.from({ length: 100 }).map((v, i) => i);
 const instance = new ArrayShuffle(() => new MathGenerator());
-
 const result = instance.shuffle(array);
 
 console.log(result);
