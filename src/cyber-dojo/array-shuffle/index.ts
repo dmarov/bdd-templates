ebaf8281841e9b60b6081e0199312c0db8f7c02c
Dmitry Marov <d.marov94@gmail.com>
2023-02-26 11:09:41 +0300

    improved shuffle

--- src/cyber-dojo/array-shuffle/index.ts
+++ src/cyber-dojo/array-shuffle/index.ts
@@ -2,14 +2,15 @@ import { ArrayShuffle } from './array-shuffle/array-shuffle';
 import { MathRandomGenerator } from './normalized-generators/math-random';
 
 const steps = 100000;
-const totals = Array.from({ length: 100 }).map(() => 0);
+const length = 100;
+const totals = Array.from({ length }).map(() => 0);
 
 for (let i = 0; i < steps; i += 1) {
-  const array = Array.from({ length: 100 }).map((v, j) => j);
+  const array = Array.from({ length }).map((v, j) => j);
   const instance = new ArrayShuffle(() => new MathRandomGenerator());
   const result = instance.shuffle(array);
 
-  for (let k = 0; k < 100; k += 1) {
+  for (let k = 0; k < length; k += 1) {
     totals[k] += result[k];
   }
 }
