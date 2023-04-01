f50b43141f2b448ba78cbc4d0c7df11e77425448
Dmitry Marov <d.marov94@gmail.com>
2023-04-01 13:57:43 +0300

    wip

--- src/xunit/errors/assert-boolean-equal-error.ts
+++ src/xunit/errors/assert-boolean-equal-error.ts
@@ -1,5 +1,8 @@
 import { TestCaseError } from './test-case-error';
 
 export class AssertBooleanEqualError extends TestCaseError {
-
+  constructor() {
+    super('AssertBooleanEqual failed');
+    this.name = 'AssertBooleanEqualError';
+  }
 }
--- src/xunit/errors/non-assert-related-error.ts
+++ src/xunit/errors/non-assert-related-error.ts
@@ -1,5 +1,8 @@
 import { TestCaseError } from './test-case-error';
 
 export class NonAssertRelatedError extends TestCaseError {
-
+  constructor() {
+    super('NonAssertRelated failed');
+    this.name = 'NonAssertRelatedError';
+  }
 }
