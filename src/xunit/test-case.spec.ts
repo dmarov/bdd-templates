72dc30fdac560ed97b0574d4592b443837719980
Dmitry Marov <d.marov94@gmail.com>
2023-04-01 14:34:08 +0300

    wip

--- src/xunit/test-case.spec.ts
+++ src/xunit/test-case.spec.ts
@@ -4,15 +4,15 @@ import { TestCase } from './test-case';
 
 /** TODO
  * MVP
- * 1. should have "not ran" state before run
- * 2. should invoke test method
- * 3. should failed state if error occured
- * 4. should have error of correct type if error occured
+ * 1. should failed state if error occured
+ * 2. should have error of correct type if error occured
+ * 3. should have succeeded state if no error occured
+ * 4. should have no error if no error occured
  */
 
 /** TODO
  * Extra features
- * 1. improve assert functions
+ * 1. add assert functions
  * 2. should have descriptive test invocation result if test with boolean assertion succeeded
  * 3. should fail test with no assertions
  * 4. run test suite
@@ -21,28 +21,6 @@ import { TestCase } from './test-case';
  */
 
 export const tests = [
-  new TestCase('should have "not ran" state before run', () => {
-    const testCase = new TestCase('should be dummy', () => {});
-
-    if (testCase.getInvocationDetails().state !== TestCaseState.NotRan) {
-      throw new Error('should have "not ran" state before run failed');
-    }
-  }),
-
-  new TestCase('should invoke test method', () => {
-    let success = false;
-
-    const testCase = new TestCase('should set flag to true', () => {
-      success = true;
-    });
-
-    testCase.run();
-
-    if (!success) {
-      throw new Error('should invoke test method failed');
-    }
-  }),
-
   new TestCase('should have failed state if error occured', () => {
     const testCase = new TestCase('should throw error', () => {
       throw new Error('this is error');
@@ -68,6 +46,34 @@ export const tests = [
 
     if (!(error instanceof TestCaseError)) {
       throw new Error('should have error of correct type if error occured failed');
+    }
+  }),
+
+  new TestCase('should have succeeded state if no error occured', () => {
+    const testCase = new TestCase('should throw error', () => {
+
+    });
+
+    testCase.run();
+
+    const { state } = testCase.getInvocationDetails();
+
+    if (state !== TestCaseState.Succeeded) {
+      throw new Error('should have succeeded state if no error occured');
+    }
+  }),
+
+  new TestCase('should have no error if no error occured', () => {
+    const testCase = new TestCase('should throw error', () => {
+      throw new Error('this is error');
+    });
+
+    testCase.run();
+
+    const { error } = testCase.getInvocationDetails();
+
+    if (!(error instanceof TestCaseError)) {
+      throw new Error('should have no error if no error occured failed');
     }
   }),
 ];
